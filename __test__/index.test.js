d5dbf3fce402a1084397d6b67059d808e7323e42
witchenh <TP030952@mail.apu.edu.my>
2021-05-22 18:34:41 +0800

    Update code

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -5,7 +5,7 @@ describe("Get route", () => {
   it("page should return hello world", async (done) => {
     const res = await request(app).get("/");
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toEqual("Hello world");
+    expect(res.body).toEqual("Hello world !!!");
     done();
   });
 });
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).json("Hello world");
+  res.status(200).json("Hello world !!!");
 });
 
 module.exports = app.listen(process.env.PORT || 4000, () =>
